d1d8cfebf2d0ff3ac07e76b098a4db859ca41734
lalitarchan03 <lalitarchanprakashpandey@gmail.com>
2024-02-01 20:58:12 +0530

    Added controller to make code more readable and manageable

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+const productController = require('../controllers/product');
+
+// /admin/add-product => GET
+router.get('/add-product', productController.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productController.postAddProduct);
+
+module.exports = router;
+
